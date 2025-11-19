3ab9e779d52f9ef98765835f6d8d2c7900d125ea
thetrokers <luisleos90@gmail.com>
2025-11-18 19:01:04 -0600

    a

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setupInterceptors } from "./interceptors";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:61648/api",
+  baseURL: "http://10.16.6.176:5002/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
