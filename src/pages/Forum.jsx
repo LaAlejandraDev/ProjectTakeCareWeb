cf9b0252c34e3d1dd7e284c271353152289d5926
LaAlejandraDev <alejandrasauceda2200@gmail.com>
2025-12-02 01:26:15 -0600

    Post completos.

--- src/pages/Forum.jsx
+++ src/pages/Forum.jsx
@@ -2,7 +2,8 @@ import { Navigate, Outlet, useLocation } from "react-router-dom";
 import SearchBar from "../components/Forum/SearchBar";
 import { useEffect, useState } from "react";
 
-export default function Forum() { // Componente principal del foro
+export default function Forum() {
+  // Componente principal del foro
   const location = useLocation();
   const [searchDisabled, setSearchDisabled] = useState(false);
 
